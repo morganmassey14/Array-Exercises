167a2e898ce357a5148ad828626dbf6639fd7ce2
tgann11 <morganne1170@gmail.com>
2021-11-11 16:00:29 -0600

    first exercise

--- src/filterExercises.js
+++ src/filterExercises.js
@@ -3,32 +3,57 @@ import { useStudents, useInstructors } from "./data/classroom.js";
 const students = useStudents();
 const instructors = useInstructors();
 
+export const getStudentsInCohort = (cohort) => {
+    return students.filter(student => student.cohort === cohort)
+}
+
 // Export a function named getStudentsInCohort
 // It should accept one integer parameter named `cohort`
 // It should return an array of just the students who are in that cohort
 
+export const getFullTimeStudents = () => {
+return students.filter(student => student.fullTime === fullTime)
+}
 // Export a function called getFullTimeStudents
 // It should not accept any parameters
 // It should return an array of only the full time students
 
+// const getFullTimeStudents = () => {
+// 
 // Export a function called getStudentsByInstructorId
 // It should accept one integer parameter name `instructorId`
 // It should return an array of students with that instructor
 
+// const getStudentsByInstructorId = () => {
+
+// 
 // Export a function called getPolyglotStudents
 // It should accept one integer parameter named `languageCount`
 // It should return an array of students who know as many (or more) languages than `languageCount`
 // Ex: If the number 2 is passed to the function, only the students who know 2 or more languages should be returned
 
+// const getPolyglotStudents = () => {
+
+// }
 // Export a function called getAvailableInstructors
 // It should not accept any parameters
 // It should return an array of instructors that don't have any students
 
+// const getAvailableInstructors = () => {
+
+// }
 // Export a function called getStudentsByLanguage
 // It should accept one string parameter named `language`
 // It should return an array of students who know the given language
 // HINT: In addition to the `filter` method, you might also look up the `some` method
 
+// const getStudentsByLanguage = () => {
+
+// }
+
+// return (
+
+// )}
 /******** ADVANCED CHALLENGE ********/
 /******** Only do this if all other tests are passing ****/
 /******** To test, uncomment the code at the bottom of tests/filter.spec.js  *****/
